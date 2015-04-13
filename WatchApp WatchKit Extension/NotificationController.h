3ee467e6c3e6ff7bc80dd79bbe75052ddc8ccfcf
MAEDA HAJIME <anakin.maeda@gmail.com>
2015-04-13 10:50:23 +0900

    WatchApp
    Apple Watch 開発環境構築手順

---
+++ WatchApp WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  WatchApp WatchKit Extension
+//
+//  Created by MAEDAHAJIME on 2015/03/26.
+//  Copyright (c) 2015年 MAEDAHAJIME. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
